855c04a60b357b726a6318c031b8d79f9311b22c
Saraali24 <saramabrouk.ali@gmail.com>
2023-11-10 08:02:56 -0800

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -11,24 +11,26 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initialize new objects """
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+
+        format_time = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
         if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, time_format)
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, format_time)
                 else:
-                    self.__dict__[k] = v
+                    self.__dict__[key] = value
         else:
             models.storage.new(self)
 
     def __str__(self):
         """Print str representation of object """
 
-        new = (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
-        return new
+        new_str = ("[{}] ({}) {}".format(self.__class__.__name__,
+                                         self.id, self.__dict__))
+        return new_str
 
     def save(self):
         """Update object's updates_at with current time """
@@ -39,8 +41,8 @@ class BaseModel:
     def to_dict(self):
         """Return __dict__ of object"""
 
-        new_dict = self.__dict__.copy()
-        new_dict["created_at"] = self.created_at.isoformat()
-        new_dict["updated_at"] = self.updated_at.isoformat()
-        new_dict["__class__"] = self.__class__.__name__
-        return new_dict
+        nw_dictionary = self.__dict__.copy()
+        nw_dictionary["created_at"] = self.created_at.isoformat()
+        nw_dictionary["updated_at"] = self.updated_at.isoformat()
+        nw_dictionary["__class__"] = self.__class__.__name__
+        return nw_dictionary
