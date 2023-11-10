c948485a6e67eaf24dfddf2e62166efe05118103
AyaTarek95 <aya.tarek213@icloud.com>
2023-11-10 10:13:57 -0800

    1st 2 tests files

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,21 @@
+from models import *
+import unittest
+from datetime import datetime
+
+
+class Test_AmenityClass(unittest.TestCase):
+    """
+    Test Amenity Class
+    """
+
+    def Set_up(self):
+        self.model = Amenity()
+        self.model.save()
+
+    def Test_Initialization(self):
+        self.assertTrue(hasattr(self.model, "name"))
+        self.assertEqual(self.model.name, "")
+
+
+if __name__ == "__main__":
+    unittest.main()
