a71fa33c70a11e236a7b6f775b8a3beb8a50492a
AyaTarek95 <aya.tarek213@icloud.com>
2023-11-11 13:26:21 -0800

    update test

---
+++ tests/test_console.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines unittests for console
+
+Unittest classes:
+    TestHBNBCommand_prompting
+"""
+import unittest
+
+
+class TestHBNBCommand_prompting(unittest.TestCase):
+    pass
