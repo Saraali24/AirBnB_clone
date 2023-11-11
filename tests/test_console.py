e4ed04a771beaa90ee4e0a4245b56a0507f02468
AyaTarek95 <aya.tarek213@icloud.com>
2023-11-11 13:34:37 -0800

    test

--- tests/test_console.py
+++ tests/test_console.py
@@ -9,8 +9,13 @@ from models import storage
 from models.engine.file_storage import FileStorage
 from console import HBNBCommand
 
+
 class TestHBNBCommand_prompting(unittest.TestCase):
     """Unittests for testing prompt"""
 
     def test_prompt_string(self):
         self.assertEqual("(hbnb) ", HBNBCommand.prompt)
+
+
+if __name__ == "__main__":
+    unittest.main()
